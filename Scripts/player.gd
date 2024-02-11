5969fec2c7bd36d2f07ffe0983f4a3333b9d4bdd
LiviRose <72127278+LiviRoseGames@users.noreply.github.com>
2024-02-11 00:33:00 -0800

    Updated Tileset

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,11 +5,11 @@ const SPEED = 200
 var astar_grid: AStarGrid2D
 var current_id_path: Array[Vector2i]
 
-@onready var animPlayer = $AnimationPlayer
+#@onready var animPlayer = $AnimationPlayer
 @onready var tileMap = $"../TileMap"
 
 func _ready() -> void:
-	animPlayer.play("Idle")
+	#animPlayer.play("Idle")
 	
 #Astar grid code
 	astar_grid = AStarGrid2D.new()
@@ -17,6 +17,19 @@ func _ready() -> void:
 	astar_grid.cell_size = Vector2(16,32)
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
 	astar_grid.update()
+	
+	for x in tileMap.get_used_rect().size.x:
+		for y in tileMap.get_used_rect().size.y:
+			var tile_position = Vector2i(
+				x + tileMap.get_used_rect().position.x,
+				y + tileMap.get_used_rect().position.y
+			)
+			
+			var tile_data = tileMap.get_cell_tile_data(0, tile_position)
+			
+			if tile_data == null or tile_data.get_custom_data("walkable") == false:
+				astar_grid.set_point_solid(tile_position)
+	
 
 func _physics_process(delta : float) -> void:
 	if current_id_path.is_empty():
@@ -30,15 +43,6 @@ func _physics_process(delta : float) -> void:
 		current_id_path.pop_front()
 
 
-#Old Movement Code
-	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		#destination = get_global_mouse_position()
-	#
-	#if position != destination:
-		#animPlayer.play("Walk")
-		#position = position.move_toward(destination, delta*SPEED)
-	#else:
-		#animPlayer.play("Idle")
 
 func _input(event):
 	if event.is_action_pressed("move") == false:
