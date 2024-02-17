63fe14540a7ca64d22af107e71e28c4d8caacc9b
LiviRose <72127278+LiviRoseGames@users.noreply.github.com>
2024-02-16 19:53:30 -0800

    Inital Commit

--- Scripts/cursor.gd
+++ Scripts/cursor.gd
@@ -1,6 +1,31 @@
 extends Area2D
 
+var overObject : bool = false
+var cursor_PlayerStop
+var playerCoordinates : Vector2
+
+signal movePlayer(sig_MoveCoordinates)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position = get_global_mouse_position()
-	
+
+#if were inside an objects area2d then we go to the object. if we are not, we go to the mouse.
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if overObject:
+			playerCoordinates = cursor_PlayerStop
+		elif not overObject:
+			playerCoordinates = get_global_mouse_position()
+		emit_signal("movePlayer", playerCoordinates)
+
+func _on_area_entered(area):
+	overObject = true
+
+
+func _on_area_exited(area):
+	overObject = false
+
+func _on_object_send_player_stop(sig_PlayerStop):
+	cursor_PlayerStop = sig_PlayerStop
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,9 +5,9 @@ const SPEED = 200
 #@onready var animPlayer = $AnimationPlayer
 @onready var nav : NavigationAgent2D = $NavigationAgent2D
 var finished = false
+var pushedToFinish = false
 
 func _ready() -> void:
-	GlobalManager.selectedObject.connect(move_to_object)
 	#setting the destination
 	nav.target_position = position
 	
@@ -18,15 +18,17 @@ func _physics_process(delta : float) -> void:
 		var direction = (nav.get_next_path_position() - global_position).normalized()
 		translate(direction * SPEED * delta)
 	
-
-func _input(event):
-	if event is InputEventMouseButton:
-		finished = false
-		nav.target_position = get_global_mouse_position()
-
+	#if finished and not pushedToFinish and global_position != nav.get_final_position():
+		#translate((nav.get_final_position() - global_position).normalized() * SPEED * delta)
+		#pushedToFinish = true
+	
 
 func _on_navigation_agent_2d_navigation_finished():
+	translate((nav.get_final_position() - global_position).normalized())
 	finished = true
 
-func move_to_object():
-	print("Moving to object")
+
+func _on_cursor_move_player(sig_MoveCoordinates):
+	nav.target_position = sig_MoveCoordinates
+	finished = false
+	pushedToFinish = false
